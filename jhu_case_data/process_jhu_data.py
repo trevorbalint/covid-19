4729fa55580b5ce0ba9b45be2f11b2436059c979
trevorbalint <tabalint@gmail.com>
2020-03-19 14:41:02 +0900

    Renaming files so purpose is more clear

--- jhu_case_data/process_jhu_data.py
+++ jhu_case_data/process_jhu_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 from google.cloud import bigquery
 
-filename = 'data.csv'
+filename = 'jhu_data.csv'
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 
 
