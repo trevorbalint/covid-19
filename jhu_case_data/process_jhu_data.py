1f13c91989873994a44b24da29cab9d1aad9b266
trevorbalint <tabalint@gmail.com>
2020-06-02 10:36:48 +0900

    Repaired jhu_case_data functions
    - They changed their data format to separate out the US states
    - Processes US states separately, then merges back in
    - Removed now-unnecessary process_states function
    - Refactored common data processes to initial_data_process()

--- jhu_case_data/process_jhu_data.py
+++ jhu_case_data/process_jhu_data.py
@@ -3,71 +3,67 @@ import pandas as pd
 import datetime
 from covid_lib import bigquery_interface as cbq
 
-filename = 'jhu_data.csv'
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/" \
-      "csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+global_filename = 'global_jhu_data.csv'
+us_filename = 'us_jhu_data.csv'
+global_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/" \
+             "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+us_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/" \
+         "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 
 
 def get_states_data():
     return pd.read_csv('states.csv')
 
 
-def get_and_save_data():
+def get_and_save_data(url, filename):
     r = requests.get(url)
     with open(filename, 'wb') as f:
         f.write(r.content)
     return True
 
 
-# The data for the states is a weird amalgam of county data and state data that isn't consistent over time
-# Extract the county data and put it into the state, since that data is available starting 3/8
-def process_states(df: pd.DataFrame):
-    states_aka = {x[1].strip(): x[0] for x in get_states_data().values}
-
-    def translate_states(value):
-        if ',' in value:
-            state = value.split(',')[-1].strip()
-            return states_aka[state]
-        else:
-            return value
-
-    # Get all US values
-    states_df = df[df['Country_Region'] == 'US']
-
-    # Convert the Province_State column to just be state, removing county/city info
-    states_df.loc[:, 'Province_State'] = states_df['Province_State'].apply(lambda x: translate_states(x))
-
-    # Sum by state and date
-    states_df = states_df.groupby(['Province_State', 'Date']).agg({'Cases': 'sum'}).reset_index()
-
-    # Re-add the Country_Region value
-    states_df.loc[:, 'Country_Region'] = 'US'
-
-    # Remove all US values from the original DF
-    df = df[df['Country_Region'] != 'US']
-
-    # Concatenate new US values with old non-US values
-    return pd.concat([df, states_df])
-
-
-def primary_process():
-    if get_and_save_data():
-        data_df = pd.read_csv(filename)
+def initial_data_process(url, filename):
+    """Takes a URL and filename. Gets the data from that URL and saves it to that filename,
+    then does basic processing like renaming columns and pivoting to be an actual time series.
+    """
+    if get_and_save_data(url, filename):
+        df = pd.read_csv(filename)
     else:
         raise IOError("Data I/O error")
 
     # Drop Lat and Long as we don't need this
-    data_df = data_df.drop(['Lat', 'Long'], axis=1)
+    # First do it for global data
+    if 'Long' in df.columns:
+        df = df.drop(['Lat', 'Long'], axis=1)
+    # Then for the US data, which has way more to drop
+    else:
+        df = df.drop(['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Lat', 'Long_', 'Combined_Key'], axis=1)
 
     # Rename columns to remove / - BQ doesn't like that
-    data_df = data_df.rename({'Province/State': 'Province_State', 'Country/Region': 'Country_Region'}, axis=1)
+    df = df.rename({'Province/State': 'Province_State', 'Country/Region': 'Country_Region'}, axis=1)
 
     # Data is in a time series where each date is a column - pivot so dates are in rows
-    pivoted_df = pd.melt(data_df, ['Province_State', 'Country_Region'], var_name='Date', value_name='Cases')
+    pivoted_df = pd.melt(df, ['Province_State', 'Country_Region'], var_name='Date', value_name='Cases')
     pivoted_df.loc[:, 'Date'] = pivoted_df['Date'].apply(lambda x: datetime.datetime.strptime(x, '%m/%d/%y').date())
 
-    # Fix US data
-    final_df = process_states(pivoted_df)
+    return pivoted_df
+
+
+def primary_process():
+    # Get the global data and drop the US - we'll process that separately
+    global_data = initial_data_process(global_url, global_filename)
+    global_data = global_data[(global_data['Country_Region'] != 'United States') &
+                              (global_data['Country_Region'] != 'US')]
+
+    # Process the US data
+    us_data = initial_data_process(us_url, us_filename)
+
+    # Get rid of the county- and city-level data - condense into state by state. Rename "US" to "United States"
+    us_data = us_data.groupby(['Country_Region', 'Province_State', 'Date']).sum().reset_index()
+    us_data.loc[:, 'Country_Region'] = 'United States'
+
+    # Combine DFs and sort. Obtain daily cases number.
+    final_df = pd.concat([global_data, us_data])
     final_df.sort_values(['Province_State', 'Country_Region'], inplace=True)
 
     final_df['Daily_Cases'] = final_df['Cases'] - \
